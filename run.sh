a7b530780f2a42c09989082caba8fb8081705629
Kylian Diers <dierskylian@proton.me>
2025-12-30 14:35:25 +0100

    Trop d'ajouts, modifications et corrections de bugs

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "=========================================="
+echo "Compilation et Lancement du Moteur RPG..."
+echo "=========================================="
+chmod +x gradlew
+./gradlew lwjgl3:run
